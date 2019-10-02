f9946cb25654971cca59c7538e1a67f55120b99e
TimmyOVO <timmyovo@qq.com>
2019-10-02 16:20:30 +0800

    update constructor

--- UltraCore-Core/src/main/java/com/github/skystardust/ultracore/core/database/models/builtin/DefaultJsonModel.java
+++ UltraCore-Core/src/main/java/com/github/skystardust/ultracore/core/database/models/builtin/DefaultJsonModel.java
@@ -1,6 +1,7 @@
 package com.github.skystardust.ultracore.core.database.models.builtin;
 
 import com.github.skystardust.ultracore.core.database.models.UltraCoreBaseModel;
+import com.github.skystardust.ultracore.core.utils.FileUtils;
 import io.ebean.EbeanServer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,14 +25,19 @@ public class DefaultJsonModel extends UltraCoreBaseModel {
     private String name;
     private String content;
 
-    public static Optional<String> getContentByName(EbeanServer ebeanServer,String name){
+    public DefaultJsonModel(EbeanServer ebeanServer, String name, Object content) {
+        this.ebeanServer = ebeanServer;
+        this.name = name;
+        this.content = FileUtils.GSON.toJson(content);
+    }
+
+    public static Optional<String> getContentByName(EbeanServer ebeanServer, String name) {
         return ebeanServer.find(DefaultJsonModel.class)
                 .where()
                 .eq("name", name)
                 .findOneOrEmpty()
                 .map(DefaultJsonModel::getContent);
     }
-
 
 
     @Override
