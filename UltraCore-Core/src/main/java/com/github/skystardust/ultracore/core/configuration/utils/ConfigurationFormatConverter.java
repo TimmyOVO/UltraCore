d9c2835ea4b35a1a6ee294830e48c14edde374ef
TimmyOVO <timmyovo@qq.com>
2019-11-30 16:50:01 +0800

    现在支持yaml和json两种配置文件的读取了

---
+++ UltraCore-Core/src/main/java/com/github/skystardust/ultracore/core/configuration/utils/ConfigurationFormatConverter.java
@@ -0,0 +1,29 @@
+package com.github.skystardust.ultracore.core.configuration.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+public final class ConfigurationFormatConverter {
+    private String content;
+
+    private ConfigurationFormatConverter(String content){
+        this.content = content;
+    }
+
+    public static ConfigurationFormatConverter fromJson(String jsonContent){
+        return new ConfigurationFormatConverter(jsonContent);
+    }
+
+    public String toJSON() throws JsonProcessingException {
+        JsonNode jsonNode = new YAMLMapper().readTree(content);
+        return jsonNode.toPrettyString();
+    }
+
+    public String toYAML() throws JsonProcessingException {
+        JsonNode jsonNodeTree = new ObjectMapper().readTree(content);
+        return new YAMLMapper().writeValueAsString(jsonNodeTree);
+    }
+}
+
--- UltraCore-Core/src/main/java/com/github/skystardust/ultracore/core/utils/FileUtils.java
+++ UltraCore-Core/src/main/java/com/github/skystardust/ultracore/core/utils/FileUtils.java
@@ -1,5 +1,7 @@
 package com.github.skystardust.ultracore.core.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.skystardust.ultracore.core.configuration.utils.ConfigurationFormatConverter;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -9,21 +11,54 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class FileUtils {
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-
+    public static boolean ENABLE_YAML_MODE = true;
+    
     public static String readFileContent(File file) {
-        StringBuilder stringBuilder = new StringBuilder();
-        try {
-            Files.newReader(file, Charset.forName("UTF-8")).lines().forEach(stringBuilder::append);
-        } catch (FileNotFoundException var3) {
-            var3.printStackTrace();
+        String json = null;
+        File yamlFile = new File(file.getPath() + ".yml");
+        if (ENABLE_YAML_MODE && yamlFile.exists()){
+            try {
+                String jsonContent = readFile(yamlFile);
+                json = ConfigurationFormatConverter.fromJson(jsonContent).toJSON();
+                writeFile(file,json);
+            } catch (FileNotFoundException | JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }else {
+            try {
+                json = readFile(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
-        return stringBuilder.toString();
+        return json;
     }
 
     public static void writeFileContent(File file, String content) {
+        if (ENABLE_YAML_MODE){
+            try {
+                writeFile(new File(file.getPath()+".yml"),ConfigurationFormatConverter.fromJson(content).toYAML());
+            } catch (JsonProcessingException ignored) {
+
+            }
+        }
+        writeFile(file,content);
+    }
+
+    static String readFile(File file) throws FileNotFoundException {
+        StringBuilder stringBuilder = new StringBuilder();
+        Files.newReader(file, StandardCharsets.UTF_8).lines().forEach(str->{
+            stringBuilder.append(str);
+            stringBuilder.append("\n");
+        });
+        return stringBuilder.toString();
+    }
+
+    static void writeFile(File file,String content){
         BufferedWriter bufferedWriter = null;
         try {
             bufferedWriter = Files.newWriter(file, Charset.forName("UTF-8"));
@@ -34,3 +69,4 @@ public class FileUtils {
         }
     }
 }
+
